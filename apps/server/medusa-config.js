309300e10ea51ced9f8d0d984e3d3afb5a29fd65
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-07-13 22:27:30 +0700

    server: add source folder

---
+++ apps/server/medusa-config.js
@@ -0,0 +1,20 @@
+import { loadEnv, defineConfig } from '@medusajs/utils';
+
+loadEnv(process.env.NODE_ENV, process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
+  admin: {
+    disable: false,
+  },
+  plugins: [],
+});
